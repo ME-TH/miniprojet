4c24d792f5f75fa93cddc67e42ab480057aa6e95
OfficielSalah <officielsalah1970@gmail.com>
2020-04-06 08:35:17 +0100

    server modification

--- client/home/app.js
+++
@@ -1,6 +0,0 @@
-let matha = document.getElementById("math");
-let algoa = document.getElementById("algo");
-let phya = document.getElementById("phy");
-let mathbu = document.getElementById("math");
-let algobu = document.getElementById("algo");
-let phybu = document.getElementById("phy");
