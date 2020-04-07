8e7bd99fed412cc80bbcade6f4c3e05c2b878d5a
OfficielSalah <officielsalah1970@gmail.com>
2020-04-07 14:07:09 +0100

    improving folder structure

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,11 @@ const express = require("express");
 const homerouter = express.Router();
 
 homerouter.get("/", (_req, res) => {
-  res.render("home", { title: "Qrayti Web App" });
+  res.render("home", {
+    title: "Qrayti Web App",
+    style: "home.css",
+    script: "home.js",
+  });
 });
 
 module.exports.route = homerouter;
